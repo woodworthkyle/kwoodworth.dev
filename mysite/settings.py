66274514631cc6cc48713d0d5ce477c4025e5eac
Kyle Woodworth <woodworthkyle@gmail.com>
2026-01-15 01:57:05 +0000

    Enabling HTTPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,7 +4,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dev-only-change-me"
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["kwoodworth.dev","127.0.0.1", "localhost"]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 INSTALLED_APPS = [
     "django.contrib.admin",
